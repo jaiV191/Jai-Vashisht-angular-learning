a8635a82f824d2891069fe7669cdc3c3379b6c41
jai <W0845065@myscc.ca>
2024-09-17 14:50:56 -0400

    made changes in package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {User} from "./models/user";
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,13 @@ export class AppComponent {
   title = 'Jai-Vashisht-angular-learning';
   name: string = "Jai vashisht";
   favPlayer: string = "Stephen Curry";
+
+  users: User[] = [
+    { id: 1, firstName: 'Michael', lastName: 'Jordan', team: 'Chicago Bulls', isAdmin: true },
+    { id: 2, firstName: 'Kobe', lastName: 'Bryant', team: 'Los Angeles Lakers' },
+    { id: 3, firstName: 'LeBron', lastName: 'James', team: 'Los Angeles Lakers' },
+    { id: 4, firstName: 'Larry', lastName: 'Bird', team: 'Boston Celtics', isAdmin: false },
+    { id: 5, firstName: 'Stephen', lastName: 'Curry', team: 'Golden State Warriors' },
+    { id: 6, firstName: 'Tim', lastName: 'Duncan', team: 'San Antonio Spurs', isAdmin: true }
+  ];
 }
