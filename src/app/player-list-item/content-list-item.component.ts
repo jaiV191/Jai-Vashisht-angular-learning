320e275a2c9307c08b96fedee8fed7f12ab44531
jai <w0845065@myscc.ca>
2024-09-25 00:56:01 -0400

    Created content list and acontent list item

---
+++ src/app/player-list-item/content-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './player-list-item.component.html',
+  styleUrl: './player-list-item.component.css'
+})
+export class ContentListItemComponent {
+
+}
---
+++ src/app/player-list/content-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './players-list.component.html',
+  styleUrl: './players-list.component.css'
+})
+export class ContentListComponent {
+
+}
