4da8c624a68982bbdd4f69bcfaef015a18169c33
jai <W0845065@myscc.ca>
2024-09-25 09:21:30 -0400

    added content list tags in html and layout the format for the future development of the code

--- src/app/player-list-item/content-list-item.component.ts
+++ src/app/player-list-item/content-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {User} from "../shared/models/user";
 
 @Component({
   selector: 'app-player-list-item',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './player-list-item.component.css'
 })
 export class ContentListItemComponent {
-
+  @Input() player?:User;
 }
--- src/app/player-list/content-list.component.ts
+++ src/app/player-list/content-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from "../shared/models/user";
 
 @Component({
   selector: 'app-player-list',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './players-list.component.css'
 })
 export class ContentListComponent {
-
+  players: User[] = [
+    { id: 1, firstName: 'Michael', lastName: 'Jordan', team: 'Chicago Bulls', mvp: 5 },
+    { id: 2, firstName: 'Kobe', lastName: 'Bryant', team: 'Los Angeles Lakers', mvp: 1 },
+    { id: 3, firstName: 'LeBron', lastName: 'James', team: 'Los Angeles Lakers',mvp:4 },
+    { id: 4, firstName: 'Larry', lastName: 'Bird', team: 'Boston Celtics',mvp:3 },
+    { id: 5, firstName: 'Stephen', lastName: 'Curry', team: 'Golden State Warriors',mvp:2 },
+    { id: 6, firstName: 'Tim', lastName: 'Duncan', team: 'San Antonio Spurs',mvp:2 }
+  ];
 }
