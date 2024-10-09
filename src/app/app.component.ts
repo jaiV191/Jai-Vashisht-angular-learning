db87aefaafcf2e79e20ca55d3d19024a7cdc5636
“Jai <w0845065@myscc.ca>
2024-10-09 10:46:58 -0400

    Completed Assignment 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {User} from "./shared/models/user";
 import {JsonPipe, NgForOf, NgIf} from "@angular/common";
+import {ContentListItemComponent} from "./player-list-item/content-list-item.component";
+import {PlayerService} from "./services/player.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, JsonPipe, NgIf],
+  imports: [RouterOutlet, NgForOf, JsonPipe, NgIf, ContentListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,6 +16,11 @@ export class AppComponent {
   title = 'Jai-Vashisht-angular-learning';
   name: string = "Jai vashisht";
   favPlayer: string = "Stephen Curry";
+
+  gswplayer?:User;
+
+  constructor(private playerService: PlayerService) {
+  }
 
   users: User[] = [
     { id: 1, firstName: 'Michael', lastName: 'Jordan', team: 'Chicago Bulls', mvp: 5 },
@@ -23,4 +30,12 @@ export class AppComponent {
     { id: 5, firstName: 'Stephen', lastName: 'Curry', team: 'Golden State Warriors',mvp:2 },
     { id: 6, firstName: 'Tim', lastName: 'Duncan', team: 'San Antonio Spurs',mvp:2 }
   ];
+
+  ngOnInit(){
+    this.playerService.getPlayerById(5).subscribe({
+      next:(data:User|undefined)=>this.gswplayer=data,
+      error:err=>console.error("Error Fetching Players By Id",err),
+      complete:()=>console.log("Player data fetch complete")
+    });
+  }
 }
